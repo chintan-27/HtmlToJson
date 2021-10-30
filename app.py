2ee7626de4857afdb2073dce3fb7d40ef016777b
Chintan Acharya <chintan.acharya27@gmail.com>
2021-10-30 12:53:29 +0530

    Cleaned Html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from os import remove
 from flask import Flask, request
 from flask import json
 from flask.json import jsonify
 from flask_cors import CORS
 import xmltodict
 import xmltojson
+from lxml.html.clean import Cleaner, clean_html
 
 app = Flask(__name__)
 CORS(app)
@@ -16,10 +18,20 @@ def helloWorld():
 
 @app.post("/convert")
 def convert():
+    cleaner = Cleaner(
+        scripts=True,
+        javascript=True,
+        page_structure=False,
+        links=False,
+        remove_tags=["img", "br"],
+    )
     string = request.get_json()["string"]
-    result = xmltodict.parse(string)
+    cleaned = cleaner.clean_html(string)
+    cleaned = cleaned.replace('"', "'")
+    result = xmltodict.parse(cleaned)
     # res = json.loads(result)
     return jsonify(result)
+    # return cleaned
 
 
 if __name__ == "__main__":
